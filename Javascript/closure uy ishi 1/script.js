c52aa858e35ee533712a850cd474657cbab2d661
Bexruz <shbehruz04@gmail.com>
2026-05-11 14:47:25 +0500

    deploy: auto update 2026-05-11 14:47

--- Javascript/closure uy ishi 1/script.js
+++ Javascript/closure uy ishi 1/script.js
@@ -2,36 +2,42 @@ const passInput = document.getElementById("passInput");
 const passResult = document.getElementById("passResult");
 const submitBtn = document.getElementById("submitBtn");
 
+const userName = document.getElementById("userName");
 const userYear = document.getElementById("userYear");
 const ageResult = document.getElementById("ageResult");
 const calculateBtn = document.getElementById("calculateBtn");
 const currentYear = 2026;
 
 function passwordChecker() {
-    return function () {
+    return () => {
         const password = passInput.value.trim();
-
         if (password.length < 8) {
             passResult.textContent = "Parol kamida 8 ta belgidan iborat bo'lishi kerak";
             passResult.style.color = "red";
             return;
         }
 
-        if (password.includes("123")) {
+        if (password.includes("1234")) {
             passResult.textContent = "Parol xavfsiz emas, 123 raqamlarini o'z ichiga olmasligi kerak";
             passResult.style.color = "red";
             return;
+        } else {
+            passResult.textContent = "Xavfsiz parol";
+            passResult.style.color = "green";
+            return;
         }
 
-        passResult.textContent = "Xavfsiz parol";
-        passResult.style.color = "green";
     };
 }
 
+
 function ageCalculator() {
 
-    return function () {
+    return () => {
         const birthYear = Number(userYear.value);
+        let name = userName.value.trim();
+        let formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+
 
         if (!userYear.value || Number.isNaN(birthYear)) {
             ageResult.textContent = "Tug'ilgan yilni kiriting";
@@ -42,13 +48,14 @@ function ageCalculator() {
         const age = currentYear - birthYear;
 
         if (age > 18) {
-            ageResult.textContent = `Siz ${age} yoshdasiz. Xush kelibsiz`;
+            ageResult.textContent = `Hurmatli ${formattedName}, Siz ${age} yoshdasiz. Xush kelibsiz`;
             ageResult.style.color = "green";
             return;
+        } else {
+            ageResult.textContent = `Hurmatli ${formattedName}, Siz ${age} yoshdasiz. Sizga kirish mumkin emas`;
+            ageResult.style.color = "red";
+            return;
         }
-
-        ageResult.textContent = `Siz ${age} yoshdasiz. Sizga kirish mumkin emas`;
-        ageResult.style.color = "red";
     };
 }
 
