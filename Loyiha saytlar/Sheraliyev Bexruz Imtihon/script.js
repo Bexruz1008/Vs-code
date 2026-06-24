7a60250e4efce0c851882619bc69fdca2623b516
Bexruz <shbehruz04@gmail.com>
2026-06-24 16:20:13 +0500

    commit

--- Loyiha saytlar/Sheraliyev Bexruz Imtihon/script.js
+++ Loyiha saytlar/Sheraliyev Bexruz Imtihon/script.js
@@ -1,3 +1,4 @@
+import {tips} from 
 // ---
 
 // ## 📝 JavaScript Imtihon (2 soat)
@@ -83,12 +84,119 @@ const students = [
   { name: "Sardor", score: 78 },
   { name: "Nilufar", score: 62 },
 ];
-
+let passedStudents = [];
 let checkResult = students.forEach((student) => {
   if (student.score > 60) {
     console.log(`${student.name} - o'tdi`);
+    passedStudents.push(student);
   } else {
     console.log(`${student.name} - yiqildi`);
   }
 });
-console.log(checkResult);
+console.log(passedStudents);
+
+const orders = [
+  {
+    id: 1,
+    product: "MacBook Pro",
+    price: 2500,
+    category: "Laptop",
+    brand: "Apple",
+    inStock: true,
+  },
+  {
+    id: 2,
+    product: "iPhone 15 Pro",
+    price: 1200,
+    category: "Telefon",
+    brand: "Apple",
+    inStock: true,
+  },
+  {
+    id: 3,
+    product: "Samsung Galaxy S24",
+    price: 900,
+    category: "Telefon",
+    brand: "Samsung",
+    inStock: false,
+  },
+  {
+    id: 4,
+    product: "Sony WH-1000XM5",
+    price: 400,
+    category: "Quloqchin",
+    brand: "Sony",
+    inStock: true,
+  },
+  {
+    id: 5,
+    product: "Dell XPS 15",
+    price: 1800,
+    category: "Laptop",
+    brand: "Dell",
+    inStock: true,
+  },
+  {
+    id: 6,
+    product: "AirPods Pro",
+    price: 250,
+    category: "Quloqchin",
+    brand: "Apple",
+    inStock: true,
+  },
+  {
+    id: 7,
+    product: "Playstation 5",
+    price: 500,
+    category: "Gaming",
+    brand: "Sony",
+    inStock: false,
+  },
+  {
+    id: 8,
+    product: "iPad Air",
+    price: 650,
+    category: "Planshet",
+    brand: "Apple",
+    inStock: true,
+  },
+  {
+    id: 9,
+    product: "Nintendo Switch",
+    price: 300,
+    category: "Gaming",
+    brand: "Nintendo",
+    inStock: true,
+  },
+  {
+    id: 10,
+    product: "Samsung Monitor",
+    price: 450,
+    category: "Monitor",
+    brand: "Samsung",
+    inStock: true,
+  },
+];
+let higherPrice = orders.filter((product) => {
+  return product.price > 500;
+});
+console.log(higherPrice);
+let upperCasedProducts = orders.map((products) => {
+  return products.product.toUpperCase();
+});
+console.log(upperCasedProducts);
+
+let inStock = orders.filter((product) => {
+  return product.inStock === true;
+});
+console.log(inStock);
+
+let apple = orders.filter((product) => {
+  return product.brand === "Apple";
+});
+console.log(apple);
+
+let umumiyNarx = orders.reduce(function (yigindi, qiymat) {
+  return yigindi + qiymat;
+});
+console.log(umumiyNarx);
